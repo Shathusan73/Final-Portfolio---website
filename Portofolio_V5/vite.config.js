ebee90d36412f9fe5c72413136d7be4aa1453cdb
Shathusan73 <shathus34@gmail.com>
2025-08-07 18:25:30 +0530

    changes

--- Portofolio_V5/vite.config.js
+++ Portofolio_V5/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or vue, etc.
+import { defineConfig } from 'vite';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
-  plugins: [react()],
- resolve: {
-    preserveSymlinks: true, // Sometimes helps with monorepos
-  } // This is important for deployment
-})
+  plugins: [
+    commonjs()
+  ]
+});
