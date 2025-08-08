ee9c54b61bba5a96707cf19ab9d918ab5564e88f
Shathusan73 <shathus34@gmail.com>
2025-08-08 11:58:55 +0530

    changes

--- Portofolio_V5/vite.config.js
+++ Portofolio_V5/vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite';
 import commonjs from '@rollup/plugin-commonjs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// recreate __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [
     commonjs()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+  ]
+  
 });
