a820ecd63ed34023b839cf86ded8da54e412c3aa
Shathusan73 <shathus34@gmail.com>
2025-08-08 12:40:56 +0530

    changes

--- Portofolio_V5/vite.config.js
+++ Portofolio_V5/vite.config.js
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite';
-import commonjs from '@rollup/plugin-commonjs';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 export default defineConfig({
-  plugins: [
-    commonjs()
-  ]
-  
-  
-});
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        nodeResolve({
+          browser: true,
+          preferBuiltins: false,
+        }),
+        commonjs(),
+      ],
+      onwarn(warning, warn) {
+        // Silence specific warnings
+        if (warning.code === 'THIS_IS_UNDEFINED') return
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return
+        warn(warning)
+      }
+    }
+  }
+})
